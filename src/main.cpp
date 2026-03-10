6db7ded4cc54f01002cbe1228c56fa9f32d489fb
kaidrose <136013335+kaidrose@users.noreply.github.com>
2026-03-10 18:14:21 -0500

    Add MenuLayer modification for GD Level Generator Mod
    Implement a modification for the MenuLayer to log a message when the GD Level Generator Mod is loaded.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) {
+            return false;
+        }
+
+        log::info("GD Level Generator Mod Loaded!");
+
+        return true;
+    }
+};
